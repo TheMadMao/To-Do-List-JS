76cec83f63e20d0c2202dae2459d40e23825ac5c
TheMadMao <bruno.jofiel2@gmail.com>
2026-06-09 01:13:59 -0600

    Build basic To-Do List interface

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('input');
+const add_button = document.querySelector('#add_btn');
+const delete_button = document.querySelector('#delete_btn');
+const ul = document.querySelector('ul');
+const new_task = document.createElement('li');
+
+add_button.addEventListener('click', () => {
+    if (input.value === '') {
+        alert('Please enter a task!');
+        return;
+    } else {
+        new_task.textContent = input.value;
+        ul.appendChild(new_task);
+        input.value = '';
+    }
+});
