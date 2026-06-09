3c48e0e82987db73f2afbebf2408df6ff4fd9aa4
TheMadMao <bruno.jofiel2@gmail.com>
2026-06-09 12:45:01 -0600

    btn Delete Corrections

--- JS/script.js
+++ JS/script.js
@@ -2,24 +2,24 @@ const input = document.querySelector('input');
 const add_button = document.querySelector('#add_btn');
 const delete_button = document.querySelector('#delete_btn');
 const ul = document.querySelector('ul');
-const new_task = document.createElement('li');
 
 add_button.addEventListener('click', () => {
     if (input.value === '') {
         alert('Please enter a task!');
         return;
     } else {
+        const new_task = document.createElement('li');
+        const delete_task = document.createElement('button');
+        delete_task.addEventListener('click', () => {
+            const taskToDelete = delete_task.parentElement;
+            ul.removeChild(taskToDelete);
+        });
+        delete_task.textContent = 'Delete';
+        delete_task.type = 'button';
+        delete_task.classList.add('delete');
         new_task.textContent = input.value;
         ul.appendChild(new_task);
+        new_task.appendChild(delete_task);
         input.value = '';
-    }
-});
-
-delete_button.addEventListener('click', () => {
-    if (ul.children.length === 0) {
-        alert('No tasks to delete!');
-        return;
-    } else {
-        ul.removeChild(ul.lastElementChild);
     }
 });
